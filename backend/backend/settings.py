90046352876b96928f2a465a9edfe954c49d90b2
Zheka1988 <kveladze.kz@mail.ru>
2023-05-29 23:12:58 -0700

    new actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,9 +68,6 @@ DATABASES = {
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432),
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-        #'NAME': '/data/db.sqlite3',
     }
 }
 
@@ -119,6 +116,4 @@ STATIC_ROOT = BASE_DIR / 'collected_static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [
-     'http://localhost:3000'
-]
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
