a54f28a26912226090d435d9b54917d06e07c9df
Zheka1988 <kveladze.kz@mail.ru>
2023-06-04 05:45:45 -0700

    deploy9

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,8 +7,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-# ALLOWED_HOSTS = ['84.201.152.223', '127.0.0.1', 'localhost', 'taskimaski.ddns.net']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.201.152.223', '127.0.0.1', 'localhost', 'taskimaski.ddns.net']
+# ALLOWED_HOSTS = []
 
 # Application definition
 
